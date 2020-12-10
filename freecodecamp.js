4658947be123e84a4c1d5976351fc4f9d3f8658b
Mohsin Miah <moemia12@gmail.com>
2020-12-10 17:22:48 +0000

    Intermediate Algorithm Scripting: Wherefore art thou

--- freecodecamp.js
+++ freecodecamp.js
@@ -76,3 +76,26 @@ function diffArray(arr1, arr2) {
     console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
     
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Intermediate Algorithm Scripting: Wherefore art thou
+// Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object // has to be present in the object from the collection if it is to be included in the returned array.
+
+// For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must // return the third object from the array (the first argument), because it contains the name and its value, that was passed on as the second argument.
+
+function whatIsInAName(collection, source) {
+
+    let keys = Object.keys(source);                                     // Find the key of the source by declaring a variable
+
+      return collection.filter(function(obj){                           // Filter through the collection to find the variable 'keys'
+        for (let key of keys){                                          // Looking within the key inside 'keys' which is currently the source (last: "capulet")
+          if(!obj.hasOwnProperty(key) || obj[key] !== source[key]){     // Checking to see if the obj doesn't have key inside the 'keys' ie (if last: "capulet" isn't inside collection)
+            return false
+    }
+  }
+
+  return true
+})
+
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
