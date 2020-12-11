204b931c064b7a74e5de8df3533418fa229828a9
Mohsin Miah <moemia12@gmail.com>
2020-12-11 03:06:53 +0000

    Intermediate Algorithm Scripting: Search and Replace

--- freecodecamp.js
+++ freecodecamp.js
@@ -112,9 +112,48 @@ function spinalCase(str) {
 
 
 
-  return replaceWithDashes.toLowerCase();                               // replaceWithDashes variable, being the final variable is transformed to lower case
+  return replaceWithDashes.toLowerCase();                             // replaceWithDashes variable, being the final variable is transformed to lower case
 }
 
 spinalCase('This Is Spinal Tap');
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Intermediate Algorithm Scripting: Search and Replace
+// Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+
+// First argument is the sentence to perform the search and replace on.
+// Second argument is the word that you will be replacing (before)
+// Third argument is what you will be replacing the second argument with (after).
+
+//Note
+// Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word "Book" with the word "dog", it should be replaced as "Dog"
+
+function myReplace(str, before, after) {
+
+  let newString = str.split(" ");                                     // Declare a new variable which holds the 'str' split into individual string values
+
+  for(let i = 0; i < newString.length; i++){                          // Iterate through the 'st' and check to see if 'before' value is present using code below
+    if(newString[i] === before){                                      // Check to see if 'before' is present in newString iteration. If present then return newString[i] to = 'after'
+
+      if(newString[i][0] === newString[i][0].toUpperCase()){          // Extra conditionals to check whether the iterated value has a capatalised initial letter
+        after = after[0].toUpperCase() + after.slice(1);
+        };
+
+      if(newString[i][0] === newString[i][0].toLowerCase()){          // Extra conditionals to check whether the iterated value has a lower cased initial letter
+        after = after[0].toLowerCase() + after.slice(1);
+        };
+
+       newString[i] = after                                           // Once all conditionals are met, newString at position [i] will be replaced with 'after'
+    }   
+  }
+
+
+  return newString.join(" ");                                         // The final string will join together using spaces (" ")
+}
+
+let result = myReplace("I think we should look up there", "up", "Down");
+
+console.log(result)
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
