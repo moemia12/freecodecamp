91d703c5cf20063bd98fd8f92174f94f37508573
Mohsin Miah <moemia12@gmail.com>
2020-12-21 21:13:16 +0000

    Intermediate Algorithm Scripting: Everything Be True

--- freecodecamp.js
+++ freecodecamp.js
@@ -464,3 +464,51 @@ let result = steamrollArray([1, [2], [3, [[4]]]]);
 console.log(result)
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Intermediate Algorithm Scripting: Binary Agents
+// Return an English translated sentence of the passed binary string.
+
+// The binary string will be space separated.
+
+function binaryAgent(str) {
+
+  return str.split(" ").map(binaryDataPoint => {                     // Initially return the str split which splits the str into individual arrays to be mapped out
+    let characterCode = parseInt(binaryDataPoint, 2)                 // characterCode will be the binary equivilent of the str with a radix of 2
+    let decipheredLetter = String.fromCharCode(characterCode)
+    return decipheredLetter;
+  }).join("");
+ 
+}
+
+let binaryData = 
+"01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111";
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Intermediate Algorithm Scripting: Everything Be True
+// Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+// In other words, you are given an array collection of objects. The predicate pre will be an object property and you need to return true if its value is truthy. Otherwise, return false.
+// In JavaScript, truthy values are values that translate to true when evaluated in a Boolean context.
+// Remember, you can access object properties through either dot notation or [] notation.
+
+function truthCheck(collection, pre) {
+
+  let result = true;                                                // Initialise the result to start at "true" , or If pre is in collection then return true 
+    for (let i = 0; i < collection.length; i++){                    // Iterate through the collection array to be compared against the predicate (pre)
+        if (                                                        // The if statement will find all "falsy" values within the collection to return the result as false 
+            Number.isNaN(collection[i][pre]) ||                     
+            collection[i][pre] === undefined ||
+            collection[i][pre] === 0 ||
+            collection[i][pre] === null ||
+            collection[i][pre] === ""  
+            ){
+              return false
+             }
+          }
+  
+   return result;
+  
+  }
+  truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
