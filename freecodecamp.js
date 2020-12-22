cc505b03302f31ad21be3cec51157873159b66bd
Mohsin Miah <moemia12@gmail.com>
2020-12-22 20:26:28 +0000

    Intermediate Algorithm Scripting: Make a Person

--- freecodecamp.js
+++ freecodecamp.js
@@ -565,3 +565,49 @@ function addTogether() {
   console.log(addTogether(2,3));
   
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Intermediate Algorithm Scripting: Make a Person
+// Fill in the object constructor with the following methods below:
+  
+  getFirstName()
+  getLastName()
+  getFullName()
+  setFirstName(first)
+  setLastName(last)
+  setFullName(firstAndLast)
+
+// Run the tests to see the expected output for each method. 
+// The methods that take an argument must accept only one argument and it has to be a string. These methods must be the only available means of interacting with the object.
+
+var Person = function(firstAndLast) {
+
+  this.getFullName = function() {                                      // This will give the full name of the object
+    return firstAndLast;
+  };
+
+  this.getFirstName = function(){                                      // This will split the array and give the first name only
+    return firstAndLast.split(" ")[0];
+  };
+
+  this.getLastName = function(){                                       // This will split the array and give the last name only
+    return firstAndLast.split(" ")[1]
+  };
+
+  this.setFullName = function(newFullName){                            // This will set the newFullName and replace the previous full name
+    return firstAndLast = newFullName
+  };
+
+  this.setFirstName = function(newFirstName){                          // This will set the new first name, replace the previous first name and use the predefined last name
+    return firstAndLast = newFirstName + " " + this.getLastName();
+  };
+
+  this.setLastName = function(newLastName){                            // This will set the new first name, replace the previous last name and use the predefined first name
+    return firstAndLast = this.getFirstName() + " " + newLastName;
+  };
+
+};
+
+var bob = new Person('Bob Ross');
+bob.getFullName();
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
