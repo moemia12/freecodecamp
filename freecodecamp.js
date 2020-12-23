747af0217be6ac61bbff74f5eb8bbe9895f3909d
Mohsin Miah <moemia12@gmail.com>
2020-12-23 02:48:22 +0000

    JavaScript Algorithms and Data Structures Projects: Palindrome Checker

--- freecodecamp.js
+++ freecodecamp.js
@@ -655,3 +655,26 @@ orbitalPeriod([
 
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// JavaScript Algorithms and Data Structures Projects: Palindrome Checker
+// Return true if the given string is a palindrome. Otherwise, return false.
+// A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+
+// Note: You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case) in order to check for palindromes.
+
+// We'll pass strings with varying formats, such as "racecar", "RaceCar", and "race CAR" among others.
+// We'll also pass strings with special symbols, such as "2A3*3a2", "2A3 3a2", and "2_A3*3#A2".
+
+function palindrome(str) {
+
+  let polishedString = str.replace(/\W+|_/g, "").toLowerCase();       // Initialise a variable by using regex to find all white spaces and underscores globally before lowercase
+  let reversedString = polishedString.split("").reverse().join("");   // Split the polished str to be reveresed and rejoined
+
+  if (polishedString != reversedString){                              // If the polished str is not the same as reversed str then return false
+    return false
+  }
+
+  return true;
+}
+
+palindrome("eye");
