795bfb7be95d309ac099b1bf3301a40aedb7a657
Mohsin Miah <moemia12@gmail.com>
2021-01-07 02:19:34 +0000

    JavaScript Algorithms and Data Structures Projects: Caesars Cipher

--- freecodecamp.js
+++ freecodecamp.js
@@ -708,3 +708,39 @@ function convertToRoman(num) {
   convertToRoman(12);
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// JavaScript Algorithms and Data Structures Projects: Caesars Cipher
+// One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
+
+// A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus 'A' ↔ 'N', 'B' ↔ 'O' and so on.
+
+// Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+
+// All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
+
+
+
+function rot13(str) {
+
+  let decodedResult = "";                                                     // Initialise an empty string
+  let alphabetStart = "ABCDEFGHIJKLM";                                        // Initialise the first 13 characters of alphabet
+  let alphabetEnd = "NOPQRSTUVWXYZ";                                          // Initialise the second 13 characters of alphabet
+  
+  for(let i = 0; i < str.length; i++){                                        // For loop to cycle through str
+    let letterToDecode = str[i];                                              // Initliase variable for str at position [i]
+  
+  if(alphabetStart.indexOf(letterToDecode) >= 0){                             // If the indexOf letterToDecode is present 
+    decodedResult += alphabetEnd[alphabetStart.indexOf(letterToDecode)];      // decodedResult will be the same position in the opposite alphabet varable
+  } else if(alphabetEnd.indexOf(letterToDecode) >= 0){
+    decodedResult += alphabetStart[alphabetEnd.indexOf(letterToDecode)];      // Vice Versa
+  } else {
+    decodedResult += letterToDecode;                                          // Otherwise the decodedResult will be the letterCode
+  }
+  }
+    
+    return decodedResult;
+  }
+  
+  console.log(rot13("SERR PBQR PNZC"));                                       // Returns FREE CODE CAMP
+  
+  
